22dd6946d72138d449abb1a47c0bca6b461e6c4a
Naz <badatnaz@gmail.com>
2021-09-30 13:07:07 +0100

    changed AWS region so that infrastructure is provisioned in Ireland

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ terraform {
 
 
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-west-1"
 }
 
 
@@ -30,7 +30,7 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-0a8e758f5e873d1c1"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
